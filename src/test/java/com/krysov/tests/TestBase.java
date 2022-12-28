103e3cf7b05ba29cd8025d6df1beafa3b6951ef4
Alex Krysov <krysov-95@mail.ru>
2022-12-28 20:05:46 +0600

    added Property

--- src/test/java/com/krysov/tests/TestBase.java
+++ src/test/java/com/krysov/tests/TestBase.java
@@ -15,10 +15,12 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("selenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
---
+++ src/test/java/com/krysov/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,5 @@
+package com.krysov.tests.properties;
+
+public class SystemPropertiesTests {
+    
+}
